fe7fa8a46e9c640e20dae1a43ff1146e807090ff
kim6394 <kim6394@hanmail.net>
2019-06-10 14:49:34 +0900

    update

--- app.js
+++ app.js
@@ -101,6 +101,6 @@ app.use((err, req, res) => {
     res.render('error');
 });
 
-app.listen(app.get('port'), () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(app.get('port'), '번 포트에서 대기중..');
 });
